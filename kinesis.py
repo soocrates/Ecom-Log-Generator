fbb9ad1019f97858d493845e2eacb43d5cddf107
soocrates <rooshan.vtech@gmail.com>
2024-06-21 00:26:03 +0545

    fetch variables from .env

--- kinesis.py
+++ kinesis.py
@@ -2,12 +2,20 @@ import json
 import boto3
 import uuid
 from main import generate_logs
+from dotenv import load_dotenv
+import os
 
-STREAM_NAME = ""
+# Load environment variables from .env file
+load_dotenv()
+
+AWS_REGION = os.getenv('AWS_REGION')
+STREAM_NAME = os.getenv('STREAM_NAME')
+LOGS_PER_SECOND = int(os.getenv('LOGS_PER_SECOND', 20))  # Default to 20 if not set
+LOGS_DURATION = int(os.getenv('LOGS_DURATION', 60))  # Default to 60 seconds if not set
 
 def get_data(per_second, duration):
     return generate_logs(per_second, duration)
-  
+
 def generate(stream_name, kinesis_client, per_second, duration):
     log_generator = get_data(per_second, duration)
     count = 0  # Initialize counter
@@ -22,9 +30,6 @@ def generate(stream_name, kinesis_client, per_second, duration):
             print(f"Record {count}: Data sent to Kinesis = {data}")  # Print the counter
         except Exception as e:
             print(f"Error sending record: {e}")
-        
 
 if __name__ == '__main__':
-    logs_per_second = int(input("Enter the number of logs per second: "))
-    duration_seconds = int(input("Enter the duration in seconds: "))
-    generate(STREAM_NAME, boto3.client('kinesis', region_name='us-east-1'), logs_per_second, duration_seconds)
+    generate(STREAM_NAME, boto3.client('kinesis', region_name=AWS_REGION), LOGS_PER_SECOND, LOGS_DURATION)
